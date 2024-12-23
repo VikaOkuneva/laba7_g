ec3906a907a99b8cb207dcb09c4908c1f953ce57
origin <Vika.okuneva.96@bk.ru>
2024-12-23 21:59:02 +0300

    var1

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onCloseButtonClick(View v) {
+        String[] mIngredients = {"Томаты", "Курица", "Шампиньоны", "Маслины"};
+        final boolean[] mSelectedIngredients = {false, false, false, false};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Выберите ингредиенты для своей пиццы")
+                .setCancelable(false)
+                .setMultiChoiceItems(mIngredients, null, new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                        mSelectedIngredients[which] = isChecked;
+                    }
+                })
+                .setPositiveButton("Ок",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
